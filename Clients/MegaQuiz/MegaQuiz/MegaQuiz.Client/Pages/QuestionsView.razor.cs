d1cef2798f90935ff9cd8e721a31479f7491ccd9
NinjaGrandpa <34811547+NinjaGrandpa@users.noreply.github.com>
2023-12-12 14:50:38 +0100

    stuff blazor

---
+++ Clients/MegaQuiz/MegaQuiz/MegaQuiz.Client/Pages/QuestionsView.razor.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http.Json;
+using Domain.Common.DTOs;
+using Microsoft.AspNetCore.Components;
+
+namespace MegaQuiz.Client.Pages;
+
+public partial class QuestionsView : ComponentBase
+{
+    private readonly HttpClient _client;
+
+    public QuestionsView(HttpClient client)
+    {
+        _client = client;
+    }
+
+    public List<QuestionDto> Questions { get; set; } = new List<QuestionDto>();
+
+    protected override async Task OnInitializedAsync()
+    {
+        Questions = await _client.GetFromJsonAsync<List<QuestionDto>>("/questions/");
+    }
+}
